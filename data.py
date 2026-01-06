173d4fd29c33341a46a1d3fafaa2a50c7d9e242a
harlyn99 <liviniakarlyn@gmail.com>
2026-01-06 10:51:44 +0700

    Add inventory data structure in data.py

---
+++ data.py
@@ -0,0 +1,11 @@
+# data.py
+
+inventory = {
+    "Makanan": {
+        "Mi Instan Ayam Bawang": [
+            {"jumlah": 2, "expired": "2026-03-20"},
+            {"jumlah": 1, "expired": "2026-01-02"},
+            {"jumlah": 2, "expired": "2026-04-06"}
+        ]
+    }
+}
